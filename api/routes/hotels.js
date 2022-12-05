1f865b48c00f5de512cefd50ad640cc005854cff
Michaels-Macbook-Pro <michaelreyesbusiness@gmail.com>
2022-12-05 18:16:09 -0500

    added JWT role based auth verification

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -1,17 +1,19 @@
 import express, { Router } from "express";
 import { createHotel, deleteHotel, getHotel, getHotels, updateHotel } from "../controllers/hotel.js";
 import Hotel from "../models/Hotel.js";
-
+import {verifyAdmin} from "../utils/verifyToken.js"
 const router = express.Router();
 
 //CREATE
-router.post('/', createHotel)
+router.post("/", verifyAdmin, createHotel);
+
 //UPDATE
-router.put('/:id', updateHotel)
+router.put("/:id", verifyAdmin, updateHotel);
 //DELETE
-router.delete('/:id', deleteHotel)
+router.delete("/:id", verifyAdmin, deleteHotel);
 //GET
-router.get('/:id', getHotel)
+
+router.get("/find/:id", getHotel);
 //GET ALL
 router.get('/', getHotels)
 export default router
