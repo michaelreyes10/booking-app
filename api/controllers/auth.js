a1f9ceba4343147067e59cbfd268fe6311d0d965
Michaels-Macbook-Pro <michaelreyesbusiness@gmail.com>
2022-12-05 17:20:15 -0500

    added cookies

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -30,9 +30,16 @@ export const register = async (req, res, next) => {
         user.password
       );
       if (!isPasswordCorrect)
-        return next(createError(400, "Wrong password or username!"));
+    return next(createError(400, "Wrong password or username!"));
 
-        res.status(200).send(user)
+        const token = jwt.sign({id: user._id, isAdmin: user.isAdmin }, process.env.JWT)
+
+const {password, isAdmin, ...otherDetails} = user._doc;
+    res.cookie("access_token", token, {
+    httpOnly: true
+})
+    .status(200)
+    .json({...otherDetails})
 
     }catch (err) {
         next(err)
